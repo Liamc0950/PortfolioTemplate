1eed018cc9b0a50e533aa028c9c91effe7b9b07e
Liam Corley <liamc0950@gmail.com>
2020-04-29 20:25:54 -0400

    Connect postgres database

--- LiamCorleyPortfolio/settings/production.py
+++ LiamCorleyPortfolio/settings/production.py
@@ -9,6 +9,17 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+#Connect to heroku Postgres Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+
+
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
