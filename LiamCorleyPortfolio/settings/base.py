923ef936cad1b1f3cb2387817a31c206fbed5e42
Liam Corley <liamc0950@gmail.com>
2020-04-30 16:58:22 -0400

    Reconfigure AWS S3

--- LiamCorleyPortfolio/settings/base.py
+++ LiamCorleyPortfolio/settings/base.py
@@ -170,11 +170,22 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
+AWS_ACCESS_KEY_ID = 'AKIAR7C2DT4WXHAXW7N6'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'lpcdesignportfolio'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'Portfolio/static'),
+]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- LiamCorleyPortfolio/settings/production.py
+++ LiamCorleyPortfolio/settings/production.py
@@ -9,14 +9,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-#AWS S3 - for media hosting
-AWS_QUERYSTRING_AUTH = False
-AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-MEDIA_URL = 'http://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-
 
 
 SECURE_SSL_REDIRECT = True
