24a1b644eaf75a59969dd4c74314ce41437fc541
Liam Corley <liamc0950@gmail.com>
2020-04-29 20:58:38 -0400

    Congfigure AWS S3 media storage

--- LiamCorleyPortfolio/settings/base.py
+++ LiamCorleyPortfolio/settings/base.py
@@ -42,6 +42,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'Portfolio.apps.PortfolioConfig',
+    'storages',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- LiamCorleyPortfolio/settings/production.py
+++ LiamCorleyPortfolio/settings/production.py
@@ -9,6 +9,13 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+#AWS S3 - for media hosting
+AWS_QUERYSTRING_AUTH = False
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['liamcorleydesignportfolio']
+MEDIA_URL = 'http://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
 
 
 
