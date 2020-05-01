80e7fde0afda79b52c189a77aaa1e44456faa45c
Liam Corley <liamc0950@gmail.com>
2020-04-30 21:03:02 -0400

    url -> root

--- LiamCorleyPortfolio/settings/base.py
+++ LiamCorleyPortfolio/settings/base.py
@@ -182,7 +182,7 @@ AWS_LOCATION = 'static'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'Portfolio/static'),
 ]
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATIC_ROOT = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
